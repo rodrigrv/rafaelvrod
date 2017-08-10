4c5967cb76fb33c2c90140adf60609601b465f80
rodrigrv <rodrigrv87@gmail.com>
2017-08-10 01:19:53 -0500

    Optimizations for Thao's website. All future projects will be in her repo from now on at createdbythao.github.io

--- Portfolio-wp/header.php
+++ Portfolio-wp/header.php
@@ -2,15 +2,19 @@
 <html>
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width initial-scale=1.0" />
-    <title>Thao Ha | Graphic Designer</title>
-    <link rel="stylesheet" type="text/css" href="css/style.css" />
-    <link rel="icon" type="image/png" href="images/logocolor.png" />
-    <script src="https://use.typekit.net/iww8nri.js"></script>
-    <script>try{Typekit.load({ async: true });}catch(e){}</script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-    <script src="http://code.jquery.com/jquery-2.1.3.min.js"></script>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width initial-scale=1.0" />
+  <title>Thao Ha | Graphic Designer</title>
+  <link rel="stylesheet" type="text/css" href="css/style.css" />
+  <link rel="icon" type="image/png" href="images/logocolor.png" />
+  <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Bitter" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://code.jquery.com/jquery-2.1.3.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.1.0.min.js"
+    integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s="
+    crossorigin="anonymous">
+    </script>
 </head>
 
 <body>
